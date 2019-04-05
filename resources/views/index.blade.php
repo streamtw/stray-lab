66d4da187ce774eb92f07fe3ddd8b8f970ba0088
Stream <stream@unisharp.com>
2019-04-06 02:15:46 +0800

    首頁試切

---
+++ resources/views/index.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Stray Lab</title>
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+  <!-- Styles -->
+  <link href="/css/app.css" rel="stylesheet">
+</head>
+<body>
+  <div class="night">
+    <div class="ground">
+      <div class="street">
+        <div class="building"></div>
+      </div>
+    </div>
+    <div class="cat"></div>
+  </div>
+
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script>
+
+  </script>
+</body>
+</html>
